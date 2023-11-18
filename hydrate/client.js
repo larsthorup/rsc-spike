baea0af82a3f475a973a7c6f6355f9ca3afe9329
Lars Thorup <lars@zealake.com>
2023-11-18 15:51:31 +0100

    embed reactJSON in HTML

--- hydrate/client.js
+++ hydrate/client.js
@@ -1,13 +1,18 @@
 const { React, ReactDOM } = globalThis;
 const { hydrateRoot } = ReactDOM;
 
-clientJSX = {
-  "$$typeof": Symbol.for("react.element"),
-  "type": "div",
-  "key": null,
-  "ref": null,
-  "props": {
-    "children": "Hello World!"
-  },
-};
+function reviveJSX(key, value) {
+  if (value === "$RE") {
+    // This is our special marker we added on the server.
+    // Restore the Symbol to tell React that this is valid JSX.
+    return Symbol.for("react.element");
+  } else if (typeof value === "string" && value.startsWith("$$")) {
+    // This is a string starting with $. Remove the extra $ added by the server.
+    return value.slice(1);
+  } else {
+    return value;
+  }
+}
+
+const clientJSX = JSON.parse(globalThis.__INITIAL_CLIENT_JSX_STRING__, reviveJSX);
 hydrateRoot(document.body, clientJSX);
